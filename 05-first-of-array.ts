d6506c4273303a5813e39d5e2079a23963ebe12d
arnokay <arnokay.work@gmail.com>
2023-12-26 21:22:24 +0200

    05-first-of-array

---
+++ 05-first-of-array.ts
@@ -0,0 +1,9 @@
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+type arr3 = [];
+
+type First<T extends any[]> = T[0];
+
+type head1 = First<arr1>;
+type head2 = First<arr2>;
+type head3 = First<arr3>;
