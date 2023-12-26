7579313688bc2f984bd4a67c61ee06353f8ddeca
arnokay <arnokay.work@gmail.com>
2023-12-26 21:21:17 +0200

    02-pick

---
+++ 02-pick.ts
@@ -0,0 +1,16 @@
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type MyPick<T, B extends keyof T> = {
+  [key in B]: T[key]
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+}
