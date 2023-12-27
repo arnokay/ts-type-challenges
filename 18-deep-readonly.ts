6af22456d64d06ae236dc9f9cbee071984005e4a
arnokay <arnokay.work@gmail.com>
2023-12-27 02:31:47 +0200

    18-deep-readonly

---
+++ 18-deep-readonly.ts
@@ -0,0 +1,25 @@
+type X = { 
+  x: { 
+    a: 1
+    b: 'hi'
+  }
+  y: 'hey'
+}
+
+type Expected = { 
+  readonly x: { 
+    readonly a: 1
+    readonly b: 'hi'
+  }
+  readonly y: 'hey' 
+}
+
+type DeepReadonly<T> = {
+  readonly [key in keyof T]: keyof T[key] extends never
+    ? T[key]
+    : DeepReadonly<T[key]>;
+};
+
+type Todo = DeepReadonly<X> // should be same as `Expected`
+
+type IsEqual = Todo extends Expected ? true : false
