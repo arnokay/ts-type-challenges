b27f81d3e53f11a742bb13d3e657508dbff554d3
arnokay <arnokay.work@gmail.com>
2023-12-27 16:54:26 +0200

    26-trim updated

--- 26-trim.ts
+++ 26-trim.ts
@@ -4,4 +4,12 @@ type Trim<T extends string> = T extends `${" "}${infer RestLeft}`
     ? Trim<RestRight>
     : T;
 
+type TrimUpdated<T extends string> = T extends
+  | `${" "}${infer Rest}`
+  | `${infer Rest}${" "}`
+  ? TrimUpdated<Rest>
+  : T;
+
 type trimmed = Trim<"  Hello World  ">; // expected to be 'Hello World'
+
+type trimmed2 = TrimUpdated<"   Hello World   ">; // expected to be 'Hello World'
